434646f285a85bf632e813abab01dff8641feb50
Fateme Arjomandi <farjomandy76@gmail.com>
2023-10-02 17:56:52 +0330

    Layout Widget ThankYouView

---
+++ lib/app/modules/doctor_appointment/views/thank_you_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ThankYouView extends StatelessWidget {
+  const ThankYouView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 335,
+          height: 520,
+          color: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
