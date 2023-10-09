432a4b72f4338d037021fcceafc42f53f9dc8506
majid <majidfathi69@gmail.com>
2023-10-09 11:04:16 +0330

    EnterDigitsTabWidget widget-testing code

--- test/forgot_pass_testing.dart
+++ test/forgot_pass_testing.dart
@@ -3,13 +3,48 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:doctor_hunt/app/modules/login/views/forgot_password.dart';
 
 void main() {
-  testWidgets('forgot password', (tester) async {
+  
+  testWidgets('enter digits tab', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: EnterDigitsTabWidget()));
+
+    // Find the HeadlineWidget by type
+    expect(find.byType(HeadlineWidget), findsOneWidget);
+
+    // Find the EnterDigitsFormWidget by type
+    expect(find.byType(EnterDigitsFormWidget), findsOneWidget);
+
+    // Find the Text widget inside the HeadlineWidget
+    expect(
+      find.descendant(
+          of: find.byType(HeadlineWidget), matching: find.byType(Text)),
+      findsAtLeastNWidgets(1),
+    );
+
+    expect(
+      find.descendant(
+          of: find.byType(EnterDigitsFormWidget),
+          matching: find.byType(TextField)),
+      findsNWidgets(4),
+    );
+
+    expect(
+      find.descendant(
+          of: find.byType(EnterDigitsFormWidget),
+          matching: find.descendant(
+            of: find.byType(ElevatedButton),
+            matching: find.text('Continue'),
+          )),
+      findsOneWidget,
+    );
+  });
+
+  testWidgets('forgot password tab', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: ForgotPassTabWidget()));
 
     // Find the HeadlineWidget by type
     expect(find.byType(HeadlineWidget), findsOneWidget);
 
-    // Find the ForgotFormWidget by type
+    // Find the ForgotPassFormWidget by type
     expect(find.byType(ForgotPassFormWidget), findsOneWidget);
 
     // Find the Text widget inside the HeadlineWidget
